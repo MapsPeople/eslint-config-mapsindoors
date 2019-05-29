8dd3e81c326fb1ca821c6fdaa34e1a1aefe550c5
Anders Mogensen <am@mapspeople.com>
2019-05-29 13:41:47 +0200

    Exports a eslint configuration that extends eslint:recommendend and adds some opinionated rules.

---
+++ index.js
@@ -0,0 +1,16 @@
+module.exports = {
+    extends: 'eslint:recommended',
+    rules: {
+        'semi': [
+            'error',
+            'always'
+        ],
+        'no-console': 'warn',
+        'no-duplicate-imports': 'error',
+        'no-alert': 'warn',
+        'no-trailing-spaces': 'warn',
+        'no-unused-vars': 'warn',
+        'no-var': 'warn',
+        'keyword-spacing': 'error'
+    }
+};
