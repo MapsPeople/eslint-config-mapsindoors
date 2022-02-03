29dea6a00c08c0c7532e17f238d2f28d34d912a6
Mark Hougaard <mark@marks.dk>
2022-02-03 13:28:41 +0100

    feat: Add updates from internal repo

--- index.js
+++ index.js
@@ -1,23 +1,55 @@
 module.exports = {
     extends: 'eslint:recommended',
+    plugins: [
+        "jsdoc"
+    ],
     rules: {
-        "no-console": 1,
-        "no-alert": 1,
-        "no-var": 2,
-        "prefer-const": 1,
-        "space-in-parens": [1, "never"],
+        "no-console": 1, // disallow the use of console
+        "no-alert": 1, // disallow the use of alert, confirm, and prompt
+        "no-var": 2, // require let or const instead of var
+        "prefer-const": 1, // require const declarations for variables that are never reassigned after declared
+        "space-in-parens": [1, "never"], // enforce consistent spacing inside parentheses
         "quotes": [1, "single"],
-        "no-trailing-spaces": 1,
-        "no-irregular-whitespace": 2,
-        "eqeqeq": 2,
-        "require-await": 1,
-        "semi": ["error", "always"],
-        "semi-spacing": 2,
-        "key-spacing": 1,
-        "space-before-blocks": 1,
+        "no-trailing-spaces": 1, // disallow trailing whitespace at the end of lines
+        "no-irregular-whitespace": 2, // disallow irregular whitespace
+        "eqeqeq": 2, // require the use of === and !==
+        "require-await": 1, // disallow async functions which have no await expression
+        "semi": ["error", "always"], // require or disallow semicolons instead of ASI
+        "semi-spacing": 2, // enforce consistent spacing before and after semicolons
+        "key-spacing": 1, // enforce consistent spacing between keys and values in object literal properties
+        "space-before-blocks": 1, // enforce consistent spacing before blocks
         'keyword-spacing': 1,
         'no-unused-vars': 1,
         'indent': ["error", 4, { "SwitchCase": 1 }],
-        'brace-style': ["error", "1tbs", { "allowSingleLine": true }]
+        'brace-style': ["error", "1tbs", { "allowSingleLine": true }],
+
+        "jsdoc/check-alignment": 1,
+        "jsdoc/check-indentation": 1,
+        "jsdoc/check-param-names": 1,
+        "jsdoc/check-property-names": 1,
+        "jsdoc/check-tag-names": 1,
+        "jsdoc/check-types": 1,
+        "jsdoc/empty-tags": 1,
+        "jsdoc/implements-on-classes": 1,
+        "jsdoc/multiline-blocks": 1,
+        "jsdoc/newline-after-description": 1,
+        "jsdoc/no-multi-asterisks": 1,
+        "jsdoc/no-undefined-types": 1,
+        "jsdoc/require-asterisk-prefix": 1,
+        "jsdoc/require-description": 1,
+        "jsdoc/require-description-complete-sentence": 1,
+        'jsdoc/require-hyphen-before-param-description': 1,
+        "jsdoc/require-jsdoc": [1, {
+            "require": { "MethodDefinition": true },
+            "checkConstructors": false,
+            "enableFixer": false
+        }],
+        "jsdoc/require-param-name": 1,
+        "jsdoc/require-param-type": 1,
+        "jsdoc/require-returns": 1,
+        "jsdoc/require-returns-check": 1,
+        "jsdoc/require-returns-type": 1,
+        "jsdoc/tag-lines": 1,
+        "jsdoc/valid-types": 1,
     }
 };
